390d6ea8318ec00c645d726c0fd89d47ed7a692f
alessandrosperotti <alessandrosperotti@studenti.unimi.it>
2018-02-11 18:25:28 +0100

    WIP Creating the list

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,8 @@ const functions = require('firebase-functions');
 const https = require('https');
 const admin = require("firebase-admin");
 const cheerio = require('cheerio');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 
 // a. the action name from the make_name Dialogflow intent
@@ -16,6 +18,16 @@ const USERNAME_ACTION = 'ask_username'
 const USERNAME = 'username';
 const NUMBER_ARGUMENT = 'number';
 const COIN_BALANCE = 0;
+
+const ACTUAL_COIN_BALANCE = 3;
+
+//Parameters that indicate che column index of the trend (1d,7d,30d)
+const ONE_HOUR_TREND = 7;
+const ONE_DAY_TREND = 8;
+const SEVEN_DAYS_TREND = 9;
+const ONE_MONTH_TREND = 10;
+
+var $;
 
 
 exports.cointrackingBalance = functions.https.onRequest((request, response) => {
@@ -56,6 +68,48 @@ ref.orderByChild("userId").equalTo(userId).on("child_added", function(snapshot) 
     }
   }
 
+  function calculateTrend(currentBalance, rowData ,trendType){
+
+    //Calculates the entire portfolio trend given the table rows, current balance and thread type (1h, 1d, 7d, 30d)
+
+    var previousBalance = 0;
+    var trend;
+    var coinBalance;
+    var trendResult;
+
+    for(var i = 0; rowData.length; i++){
+
+      switch(trendType){
+        case ONE_HOUR_TREND:
+        trend = parseFloat(rowData[i].t0);
+        break;
+
+        case ONE_DAY_TREND:
+        trend = parseFloat(rowData[i].t1);
+        break;
+
+        case SEVEN_DAYS_TREND:
+        trend = parseFloat(rowData[i].t7);
+        break;
+
+        case ONE_MONTH_TREND:
+        trend = parseFloat(rowData[i].t30);
+        break;
+
+      }
+
+      coinBalance = parseFloat(rowData[i].f);
+      previousBalance += coinBalance/100 * (100 - trend); 
+
+
+    }
+
+    trendResult = ((parseFloat(currentBalance)/previousBalance)*100) - 100;
+
+    return trendResult;
+
+  }
+
   function checkUser(app){
 
     let username = app.getArgument(USERNAME);
@@ -88,6 +142,11 @@ ref.orderByChild("userId").equalTo(userId).on("child_added", function(snapshot) 
       path: '/portfolio/'+ username + '/'
     };
 
+    var tableOptions = {
+      host: 'cointracking.info',
+      path: '/ajax/portfolio_current_balance.php?portfolio='+username+'&lang=en&fiat=0&zerobalance=0'
+    }
+
     console.log("https://cointracking.info/portfolio/"+username+'/');
 
     https.get(options, (resp) => {
@@ -102,27 +161,73 @@ ref.orderByChild("userId").equalTo(userId).on("child_added", function(snapshot) 
   // The whole response has been received. Print out the result.
   resp.on('end', () => {
     console.log("Loaded successfully");
-    const $ = cheerio.load(data);
+
+    $ = cheerio.load(data);
 
     //Prendo dati
     var resultBalance = $('span.utitel_gross').first().text();
     var resultBalanceBTC = $('span.utitel_gross_grau').first().text();
-    var resultTrend = $("span[title='24h trend']").first().text();
 
-    //resultTrend è null, come mai?
+    //Making ajax call to get JSON containing data table
+    https.get(tableOptions, (resp) => {
 
-    var trendWord = resultTrend.charAt(0) == '+' ? "an increase" : "a decrease";
+      let dataTable = '';
+
+      resp.on('data', (chunk) => {
+        data+=chunk;
+      });
+
+      resp.on('error', (err) => {
+        //Build very basic response
+      });
+
+
+      resp.on('end', () => {
+
+        var jsonData = data.data; //PARSE
+        var htmlRowData;
+
+          var rows = $('#all_currencies_info tbody tr');
+    //rows.add('tr');
+
+    console.log('rows length is '+ jsonData.length);
+
+    var resultTrend = calculateTrend(jsonData, rows, ONE_DAY_TREND);
+    var trendWord = resultTrend > 0 ? "an increase" : "a decrease";
 
     if(resultBalance.length > 0){
 
-           app.ask(app.buildRichResponse()
+      //Creating List
+      var balanceList = app.buildList('My Portfolio');
+      var coinName; // = table('span.t_coin').find('span.s grau2').first().text();
+      var coinSymbol; // = table('span.t_coin').find('a').first().text;
+
+      for (var i = 0; i < jsonData.length; i++) {
+
+        htmlRowData = cheerio.load(jsonData[i].c);
+        coinName = htmlRowData('span.s grau2').first().text();
+        coinSymbol = htmlRowData('span.t_coin').find('a').first().text();
+
+
+        balanceList = balanceList.addItems(app.buildOptionItem(coinName,
+        [coinName, coinName, coinName])
+      .setTitle(coinName + " - "+ coinSymbol)
+          .setImage('https://cointracking.info/img/coins/' + coinSymbol.toLowerCase() + '.svg')
+          );
+
+      };
+
+           app.askWithList(app.buildRichResponse()
     // Create a basic card and add it to the rich response
     .addSimpleResponse("Hello, "+username+"!")
 
     .addSimpleResponse( "Your balance is "+resultBalance+".\n"+
-      "There has been "+trendWord+" of "+resultTrend.substr(1,4)+" percent!")
+      "There has been "+trendWord+" of "+resultTrend+" percent!")
 
-    .addBasicCard(app.buildBasicCard('42 is an even composite number. It' +
+    //it has increased/decreased of x since yesterday?
+    , balanceList
+
+  /*  .addBasicCard(app.buildBasicCard('42 is an even composite number. It' +
       'is composed of three distinct prime numbers multiplied together. It' +
       'has a total of eight divisors. 42 is an abundant number, because the' +
       'sum of its proper divisors 54 is greater than itself. To count from' +
@@ -133,18 +238,24 @@ ref.orderByChild("userId").equalTo(userId).on("child_added", function(snapshot) 
       .setImageDisplay('DEFAULT')
       .addSuggestions(['What are my last trades?',
         'How my portfolio is distribuited?',
-        'Another suggestion chip here'])
+        'Another suggestion chip here'])*/
+
+    
+
   );
     //Fai list con varie monete anzichè basic card??
            //Prendi i dati delle monete in percentuale e crea pie chart + altre cose da fare brain storming
-
-
     }
     else{
       app.tell("There was an error while retrieving your balance. Please try later.")
     }
 
-  });
+
+      }
+
+    });
+
+   });
 
 }).on("error", (err) => {
   console.log("Error: " + err.message);
@@ -157,7 +268,6 @@ else{
   app.tell("I can't seem to find a portfolio with that username.");
 }
 });
-
   }
   // d. build an action map, which maps intent names to functions
   let actionMap = new Map();
